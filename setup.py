9ab4bb0debb4a48d78950e1a8f95030a9856539f
ujjwal-basnet <ub.lovetocode@gmail.com>
2024-01-01 08:35:53 +0545

    cookiecutter initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project will help recconmend the top 3 food items on swiggy home page.',
+    author='ujjwal basnet',
+    license='',
+)
